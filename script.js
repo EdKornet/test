37feed48f715cfdee593d920ca198902c1dca1ac
ED <EdKornet@gmail.com>
2019-05-20 11:46:53 +0300

    My first commit here

---
+++ script.js
@@ -0,0 +1,246 @@
+document.getElementById("num1").addEventListener('click',(e)=>{
+    let id = e.target.innerHTML;
+    let display = parseInt(document.getElementById("display").innerHTML);
+    let length = document.getElementById("display").innerHTML.length;
+   console.log(typeof id);
+   if(display === 0){
+        document.getElementById('display').innerHTML = "";
+    }
+
+    document.getElementById('display').innerHTML += id;
+
+   let count =  document.getElementById('display').innerHTML;
+   count = count.length;
+    console.log(count);
+
+let number = Number.MAX_SAFE_INTEGER + "";
+    console.log(number.length);
+   if(length > number.length){
+
+   }
+});
+
+document.getElementById("num2").addEventListener('click',(e)=>{
+    let id = e.target.innerHTML;
+    let display = parseInt(document.getElementById("display").innerHTML);
+    let length = display.length;
+    console.log(id);
+    console.log(display);
+    console.log(typeof display);
+    if(display === 0){
+        document.getElementById('display').innerHTML = "";
+    }
+
+    document.getElementById('display').innerHTML += id;
+});
+
+document.getElementById("num3").addEventListener('click',(e)=>{
+    let id = e.target.innerHTML;
+    let display = parseInt(document.getElementById("display").innerHTML);
+    let length = display.length;
+    console.log(id);
+    console.log(display);
+    console.log(typeof display);
+    if(display === 0){
+        document.getElementById('display').innerHTML = "";
+    }
+
+    document.getElementById('display').innerHTML += id;
+});
+
+document.getElementById("num4").addEventListener('click',(e)=>{
+    let id = e.target.innerHTML;
+    let display = parseInt(document.getElementById("display").innerHTML);
+    let length = display.length;
+    console.log(id);
+    console.log(display);
+    console.log(typeof display);
+    if(display === 0){
+        document.getElementById('display').innerHTML = "";
+    }
+
+    document.getElementById('display').innerHTML += id;
+});
+
+document.getElementById("num5").addEventListener('click',(e)=>{
+    let id = e.target.innerHTML;
+    let display = parseInt(document.getElementById("display").innerHTML);
+    let length = display.length;
+    console.log(id);
+    console.log(display);
+    console.log(typeof display);
+    if(display === 0){
+        document.getElementById('display').innerHTML = "";
+    }
+
+    document.getElementById('display').innerHTML += id;
+});
+
+document.getElementById("num6").addEventListener('click',(e)=>{
+    let id = e.target.innerHTML;
+    let display = parseInt(document.getElementById("display").innerHTML);
+    let length = display.length;
+    console.log(id);
+    console.log(display);
+    console.log(typeof display);
+    if(display === 0){
+        document.getElementById('display').innerHTML = "";
+    }
+
+    document.getElementById('display').innerHTML += id;
+});
+
+document.getElementById("num7").addEventListener('click',(e)=>{
+    let id = e.target.innerHTML;
+    let display = parseInt(document.getElementById("display").innerHTML);
+    let length = display.length;
+    console.log(id);
+    console.log(display);
+    console.log(typeof display);
+    if(display === 0){
+        document.getElementById('display').innerHTML = "";
+    }
+
+    document.getElementById('display').innerHTML += id;
+});
+
+document.getElementById("num8").addEventListener('click',(e)=>{
+    let id = e.target.innerHTML;
+    let display = parseInt(document.getElementById("display").innerHTML);
+    let length = display.length;
+    console.log(id);
+    console.log(display);
+    console.log(typeof display);
+    if(display === 0){
+        document.getElementById('display').innerHTML = "";
+    }
+
+    document.getElementById('display').innerHTML += id;
+});
+
+document.getElementById("num9").addEventListener('click',(e)=>{
+    let id = e.target.innerHTML;
+    let display = parseInt(document.getElementById("display").innerHTML);
+    let length = display.length;
+    console.log(id);
+    console.log(display);
+    console.log(typeof display);
+    if(display === 0){
+        document.getElementById('display').innerHTML = "";
+    }
+
+    document.getElementById('display').innerHTML += id;
+});
+
+document.getElementById("num0").addEventListener('click',(e)=>{
+    let id = e.target.innerHTML;
+    let display = parseInt(document.getElementById("display").innerHTML);
+    let length = display.length;
+    console.log(id);
+    console.log(display);
+    console.log(typeof display);
+    if(display === 0){
+        document.getElementById('display').innerHTML = "";
+    }
+
+    document.getElementById('display').innerHTML += id;
+});
+
+
+document.getElementById("procent").addEventListener('click',(e)=>{
+    let id = e.target.innerHTML;
+    let display = parseInt(document.getElementById("display").innerHTML);
+    let length = display.length;
+    console.log(id);
+    console.log(display);
+    console.log(typeof display);
+    if(display === 0){
+        document.getElementById('display').innerHTML = "";
+    }
+
+    document.getElementById('display').innerHTML += id;
+});
+
+document.getElementById("division").addEventListener('click',(e)=>{
+    let id = e.target.innerHTML;
+    let display = parseInt(document.getElementById("display").innerHTML);
+    let length = display.length;
+    console.log(id);
+    console.log(display);
+    console.log(typeof display);
+    if(display === 0){
+        document.getElementById('display').innerHTML = "";
+    }
+
+    document.getElementById('display').innerHTML += id;
+});
+
+document.getElementById("multiplication").addEventListener('click',(e)=>{
+    let id = e.target.innerHTML;
+    let display = parseInt(document.getElementById("display").innerHTML);
+    let length = display.length;
+    console.log(id);
+    console.log(display);
+    console.log(typeof display);
+    if(display === 0){
+        document.getElementById('display').innerHTML = "";
+    }
+
+    document.getElementById('display').innerHTML += id;
+});
+
+document.getElementById("point").addEventListener('click',(e)=>{
+    let id = e.target.innerHTML;
+    let display = parseInt(document.getElementById("display").innerHTML);
+    let length = display.length;
+    console.log(id);
+    console.log(display);
+    console.log(typeof display);
+    if(display === 0){
+        document.getElementById('display').innerHTML = "";
+    }
+
+    document.getElementById('display').innerHTML += id;
+});
+
+document.getElementById("minus").addEventListener('click',(e)=>{
+    let id = e.target.innerHTML;
+    let display = parseInt(document.getElementById("display").innerHTML);
+    let length = display.length;
+    console.log(id);
+    console.log(display);
+    console.log(typeof display);
+    if(display === 0){
+        document.getElementById('display').innerHTML = "";
+    }
+
+    document.getElementById('display').innerHTML += id;
+});
+
+document.getElementById("plus").addEventListener('click',(e)=>{
+    let id = e.target.innerHTML;
+    let display = parseInt(document.getElementById("display").innerHTML);
+    let length = display.length;
+    console.log(id);
+    console.log(display);
+    console.log(typeof display);
+    if(display === 0){
+        document.getElementById('display').innerHTML = "";
+    }
+
+    document.getElementById('display').innerHTML += id;
+});
+
+document.getElementById("equal").addEventListener('click',(e)=>{
+    let id = e.target.innerHTML;
+    let display = parseInt(document.getElementById("display").innerHTML);
+    let length = display.length;
+    console.log(id);
+    console.log(display);
+    console.log(typeof display);
+    if(display === 0){
+        document.getElementById('display').innerHTML = "";
+    }
+
+    document.getElementById('display').innerHTML += id;
+});
